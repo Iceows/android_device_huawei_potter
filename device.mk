843ac3ea50c0fd781678540c7d2eaee09cf13b60
Iceows <mounierr07@gmail.com>
2024-08-10 13:00:05 +0200

    potter: Set bootanimation resolution

--- device.mk
+++ device.mk
@@ -8,6 +8,10 @@
 # Inherit from kirin710-9-common
 $(call inherit-product, device/huawei/kirin710-9-common/common.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
