ec74970e98fd416940d34f7b03180cc3f8b92237
Iceows <mounierr07@gmail.com>
2024-08-10 12:58:31 +0200

    potter: Set shipping api level

--- device.mk
+++ device.mk
@@ -8,6 +8,9 @@
 # Inherit from kirin710-9-common
 $(call inherit-product, device/huawei/kirin710-9-common/common.mk)
 
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 28
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
