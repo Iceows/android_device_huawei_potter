18d41b06f4ca46121b9b5985f864b34be1d0b5a6
Iceows <mounierr07@gmail.com>
2024-08-10 15:33:28 +0200

    potter: overlay: Set default Settings.Global.DEVICE_NAME according to EMUI

--- device.mk
+++ device.mk
@@ -15,6 +15,10 @@ TARGET_SCREEN_WIDTH := 1080
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
