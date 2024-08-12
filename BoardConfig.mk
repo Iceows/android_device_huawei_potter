1d0220188453a0bcf2c9024ac9dfe9ce3a52ab04
Iceows <mounierr07@gmail.com>
2024-08-12 09:05:52 +0200

    potter: Add NFC for POT-L21 device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,11 +9,15 @@ DEVICE_PATH := device/huawei/potter
 # Inherit from kirin710-common
 include device/huawei/kirin710-9-common/BoardConfigCommon.mk
 
+# Inherit the proprietary files
+include vendor/huawei/potter/BoardConfigVendor.mk
+
 # Display
 TARGET_SCREEN_DENSITY := 415
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-# Inherit the proprietary files
-include vendor/huawei/potter/BoardConfigVendor.mk
+# SKUs
+ODM_MANIFEST_SKUS += POT-L21
+ODM_MANIFEST_POT-L21_FILES := $(DEVICE_PATH)/manifest_dualsimnfc.xmlc
