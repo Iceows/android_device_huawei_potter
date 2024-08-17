67747d5949941472578858da7dfe4c5cba73eccf
Raphael Mounier <mounierr07@gmail.com>
2024-08-17 15:29:52 +0200

    potter: Change build desc and boardname

--- lineage_potter.mk
+++ lineage_potter.mk
@@ -8,6 +8,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
 
 # Inherit from potter device
 $(call inherit-product, device/huawei/potter/device.mk)
@@ -22,11 +25,14 @@ PRODUCT_BRAND := HWPOT-H
 PRODUCT_MODEL := POT-LX1
 PRODUCT_MANUFACTURER := HUAWEI
 
+# Match stock value: ro.product.board=POT-LX1
+TARGET_BOOTLOADER_BOARD_NAME := POT-LX1
+
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
 
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=POT \
-    PRIVATE_BUILD_DESC="POT-L21-user 9.1.0 HUAWEIPOT-L21 314-LGRP2-OVS release-keys"
+    PRIVATE_BUILD_DESC="POT-LX1-user 9 HUAWEIPOT-LX1 314-OVS-LGRP2 release-keys"
 
 BUILD_FINGERPRINT := HUAWEI/POT-LX1/HWPOT-H:9/HUAWEIPOT-L21/314C432:user/release-keys
